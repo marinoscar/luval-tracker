b2b11c0c924325722ef8c09b910f825a6aa36fa6
Oscar Marin <oscar@marin.cr>
2016-09-03 14:56:40 -0600

    configuration improvements

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log('Hello World');
+import * as express from "express";
+var portNum = 3000;
+
+var app = express();
+
+app.get('/', (req : express.Request, res : express.Response) => {
+    res.send('<h1>Marin</h1>');
+});
+
+app.listen(portNum, () =>{
+    console.log(`Listening in port ${portNum}`);
+});
